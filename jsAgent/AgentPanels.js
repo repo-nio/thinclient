6413a278bfa8b4ced008446cba32a390a1e1e380
Shrikant <s.sharma@nixxis.com>
2022-09-01 18:18:38 +0200

    do not show 403 when logout

--- jsAgent/AgentPanels.js
+++ jsAgent/AgentPanels.js
@@ -1322,7 +1322,7 @@ var crAgentLogout =
 
 		// debugger;
 
-		// expireActiveCookies('contactroutehttpserversessionid');
+		expireActiveCookies('contactroutehttpserversessionid');
 		// window.location.href = "CrAgentlogin.htm";
 		// window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf('/') + 1) + 'CrLoginScreen.htm';
 		location.reload();
