ae209395d4c14421ac6c335f81e457f1d7fb3ffe
Shrikant <s.sharma@nixxis.com>
2023-03-07 07:37:25 +0100

    expand collapse on double click as fat

--- assets/js/V3Functions.js
+++ assets/js/V3Functions.js
@@ -12,7 +12,7 @@ function expandCollapseSearchModeAccord()
 
     var wrap2 = document.querySelectorAll('.accordionModal .acclink');  
     wrap2.forEach(e => {
-        e.onclick = accSearchModelink_OnClick;
+        e.ondblclick = accSearchModelink_OnClick;
         removeElementClass(e, 'active');
     });
 };
@@ -62,7 +62,7 @@ function expandCollapseDispositionAccord()
     var wrap2 = document.querySelectorAll('.accordionDisposition .acclink');  
     wrap2.forEach(e => 
     {
-        if(e.classList.contains('acclink')) e.onclick = accDispositionlink_OnClick;
+        if(e.classList.contains('acclink')) e.ondblclick = accDispositionlink_OnClick;
         removeElementClass(e, 'active');
     });
 };
